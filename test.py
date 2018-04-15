269cfda83014822fb491dfc60e89894b105c8825
ayistar <824476660@qq.com>
2018-04-15 21:25:05 +0800

    hh

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ from face_detection import *
 #get_my_face(12000, 'my_faces')
 #get_other_faces('samples', 'other_faces')
 
-train('./test/my_faces', './test/other_faces', './test/train_result_5000/', max=5000)
+#train('./test/my_faces', './test/other_faces', './test/train_result_5000/', max=5000)
 
-#recognize_face('./test')
+recognize_face('./test/train_result_5000')
